92f26f9e27fa5b3c391b64478815baa623e32053
Sam Earle <samearle@fb.com>
2023-01-31 23:06:15 +0000

    Fix gitignore

---
+++ utils.py
@@ -0,0 +1,44 @@
+import cv2
+import imageio
+import numpy as np
+
+def save_video(frames, video_path, fps=10):
+    """Save a list of frames to a video file.
+    Args:
+        frames (list): list of frames to save
+        video_path (str): path to save the video
+        fps (int): frame rate of the video
+    """
+    imageio.mimwrite(video_path, frames, fps=25, quality=8, macro_block_size=1)
+
+def draw_triangle(im, pos, rot, color, tile_size):
+    """Draw a triangle on a tile grid.
+    Args:
+        im (np.ndarray): the image to draw on
+        pos (tuple): the position of the triangle
+        rot (int): the rotation of the triangle
+        color (tuple): the color of the triangle
+        tile_size (int): the size of a tile
+    Returns:
+        np.ndarray: the image with the triangle drawn"""
+    pos = pos[1], pos[0] + 2
+
+    # Define the triangle
+    triangle = np.array([
+        [0, 0],
+        [tile_size, 0],
+        [tile_size / 2, tile_size],
+    ])
+
+    # Rotate the triangle
+    rot_mat = cv2.getRotationMatrix2D((tile_size / 2, tile_size / 2), (rot - 1) * 90, 1)
+    triangle = cv2.transform(triangle.reshape(1, -1, 2), rot_mat).reshape(-1, 2)
+
+    # Draw the triangle
+    triangle = triangle.astype(np.int32)
+    # Convert color so cv2 doesn't complain
+    color = tuple([int(c) for c in color])
+
+    cv2.fillConvexPoly(im, triangle + np.array(pos) * tile_size, color)
+
+    return im
