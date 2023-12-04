12704dedfdad6adec63ba336c048ce87a083bc1b
Sam Earle <smearle93@gmail.com>
2023-12-04 17:49:53 -0500

    Debugging jax BFS

--- search_agent.py
+++ search_agent.py
@@ -26,40 +26,37 @@ FRONTIER_VMAP_SIZE = 1_000
 class SearchNode:
     state: EnvState
     action_seq: Iterable[int]
-    reward: float
     done: bool
 
 
 @partial(jax.jit, static_argnums=(0,))
-def expand_frontier(env: PlayEnv, next_frontier: SearchNode, params: EnvParams, possible_actions: jnp.ndarray):
+def expand_frontier(env: PlayEnv, next_frontier: SearchNode, params: EnvParams,
+                    possible_actions: jnp.ndarray):
 
-    def apply_action(key, state, action, params, action_seq, rew):
+    def apply_action(key, state, action, params, action_seq):
         state, obs, s_rew, done, info = env.step_env(key=key, action=action, state=state, params=params)
         action_seq = action_seq.at[state.n_step-1].set(action)
-        rew += s_rew
-        return SearchNode(state, action_seq, rew, done)
+        return SearchNode(state, action_seq, done)
 
-    def expand_frontier_node(key, state, action_seq, rew):
+    def expand_frontier_node(key, state, action_seq):
         step_key = jax.random.split(key, possible_actions.shape[0])
         nodes = \
-            jax.vmap(apply_action, in_axes=(None, None, 0, None, None, None))(
+            jax.vmap(apply_action, in_axes=(None, None, 0, None, None))(
                 step_key,
                 state,
                 possible_actions,
                 params,
                 action_seq,
-                rew,
             )
         return nodes
 
-    frontier = jax.vmap(expand_frontier_node, in_axes=(None, 0, 0, 0))(jax.random.PRNGKey(0), next_frontier.state, next_frontier.action_seq, next_frontier.reward)
+    frontier = jax.vmap(expand_frontier_node, in_axes=(None, 0, 0))(
+        jax.random.PRNGKey(0), next_frontier.state, next_frontier.action_seq)
 
     # Flatten the frontier/possible-actions dimensions so the first dimension corresponds to newly generated states
     frontier = jax.tree_map(lambda x: jnp.reshape(x, (-1, *x.shape[2:])), frontier)
 
     return frontier
-
-
 
 
 def solve(env: PlayEnv, state: EnvState, params: EnvParams,
@@ -68,7 +65,7 @@ def solve(env: PlayEnv, state: EnvState, params: EnvParams,
 
     key = jax.random.PRNGKey(0)
     action_seq = jnp.empty(max_episode_steps, dtype=jnp.int32)
-    frontier = SearchNode(state, action_seq, 0, False)
+    frontier = SearchNode(state, action_seq, 0)
     # Add a batch dimension to the frontier
     frontier = jax.tree_map(lambda x: jnp.expand_dims(x, 0), frontier)
 
@@ -88,16 +85,7 @@ def solve(env: PlayEnv, state: EnvState, params: EnvParams,
     possible_actions = jnp.array(list(range(env.action_space.n)), dtype=jnp.int32)
     n_actions = possible_actions.shape[0]
 
-    while n_iter == 0 or next_frontier.reward.shape[0] > 0:
-
-        if n_iter > 0:
-            # best_n_idxs = jnp.argpartition(next_frontier.reward, -min(next_frontier.reward.shape[0], FRONTIER_VMAP_SIZE))
-            frontier_size = len(best_n_idxs)
-
-            # Pad this out with the first idx to be size of FRONTIER_VMAP_SIZE
-            best_n_idxs = jnp.pad(best_n_idxs, (0, FRONTIER_VMAP_SIZE - len(best_n_idxs)), mode='constant', constant_values=best_n_idxs[0])
-
-            next_frontier = jax.tree_map(lambda x: x[best_n_idxs], next_frontier)
+    while n_iter == 0 or next_frontier.state.ep_rew.shape[0] > 0:
 
         # Expand the frontier
         # print(f'pre-expand frontier')
@@ -108,54 +96,72 @@ def solve(env: PlayEnv, state: EnvState, params: EnvParams,
         idxs_to_keep = []
         idxs_to_delete = []
 
-        for i in range(frontier.reward.shape[0]):
+        for i in range(frontier.done.shape[0]):
             # Can't have expanded more states than this
             if i == frontier_size * n_actions:
-                idxs_to_delete += list(range(i, frontier.reward.shape[0]))
+                idxs_to_delete += list(range(i, frontier.done.shape[0]))
                 break
             state = jax.tree_map(lambda x: x[i], frontier.state)
             hashed_state = hash(env, state)
-            if hashed_state not in visited or frontier.reward[i] > visited[hashed_state]:
+            # print(f'hashed state {hashed_state}')
+            if hashed_state not in visited or frontier.state.ep_rew[i] > visited[hashed_state]:
                 if not frontier.done[i]:
                     idxs_to_keep.append(i)
                 else:
                     idxs_to_delete.append(i)
-                visited[hashed_state] = frontier.reward[i]
-                if frontier.reward[i] > best_reward:
-                    # print(f'New best reward {frontier.reward[i]}')
+                    # print(f'Deleting state because done')
+                visited[hashed_state] = frontier.state.ep_rew[i]
+                if frontier.state.ep_rew[i] > best_reward:
+                    print(f'New best reward {frontier.state.ep_rew[i]}')
                     best_state_actions = (state, frontier.action_seq[i])
-                    best_reward = frontier.reward[i]
+                    best_reward = frontier.state.ep_rew[i]
                     n_iter_best = n_iter
             else:
+                # print('deleting state because already visited')
                 idxs_to_delete.append(i)
 
-        n_iter += frontier_size * n_actions
-        if n_iter > max_steps:
-            break
 
         # FIXME:Remove all things from the frontier that we've already visited
         # frontier = jax.tree_map(lambda x: x[jnp.array(idxs_to_keep, dtype=jnp.int32)], frontier) 
-        frontier = jax.tree_map(lambda x: jnp.delete(x, jnp.array(idxs_to_delete, dtype=jnp.int32), axis=0), frontier)
-
-        # jax.debug.print('frontier size {frontier_size}', frontier_size=frontier.reward.shape)
+        frontier = jax.tree_map(
+            lambda x: jnp.delete(x, 
+                                 jnp.array(idxs_to_delete, dtype=jnp.int32),
+                                 axis=0),
+            frontier)
 
         # Combine with the queued frontier
         total_frontier = jax.tree_map(lambda x, y: jnp.concatenate((x, y)), frontier, queued_frontier)
 
-        best_idxs = jnp.argpartition(total_frontier.reward, -min(total_frontier.reward.shape[0], FRONTIER_VMAP_SIZE))
-        
-        # if frontier.reward.shape[0] > FRONTIER_VMAP_SIZE:
-        #     # Split the frontier into a batch of size FRONTIER_VMAP_SIZE and a remainder (queue frontier)
-        #     next_frontier = jax.tree_map(lambda x: x[:FRONTIER_VMAP_SIZE], frontier)
-        #     new_queued_frontier = jax.tree_map(lambda x: x[FRONTIER_VMAP_SIZE:], frontier)
-        #     queued_frontier = jax.tree_map(lambda x, y: jnp.concatenate((x, y)), queued_frontier, new_queued_frontier)
-        # elif frontier.reward.shape[0] < FRONTIER_VMAP_SIZE:
-        #     # Add some of the queue frontier to the next frontier
-        #     front_size = frontier.reward.shape[0]
-        #     next_frontier = jax.tree_map(lambda x, y: jnp.concatenate((x, y[:FRONTIER_VMAP_SIZE - x.shape[0]])), frontier, queued_frontier)
-        #     queued_frontier = jax.tree_map(lambda x: x[FRONTIER_VMAP_SIZE - front_size:], queued_frontier)
+        # Break if we have nothing left
+        if total_frontier.done.shape[0] == 0:
+            break
 
-        print(f'iter {n_iter}, frontier size {frontier.reward.shape} queued frontier size {queued_frontier.reward.shape}')
+        best_idxs = jnp.argpartition(total_frontier.state.ep_rew, 
+                                     -min(total_frontier.done.shape[0], FRONTIER_VMAP_SIZE))
+        # Flip the order so the best idxs are first
+        best_idxs = best_idxs[::-1]
+
+        frontier_size = min(len(best_idxs), FRONTIER_VMAP_SIZE)
+
+        n_iter += frontier_size
+        if n_iter > max_steps:
+            break
+
+        # Pad this out with the first idx to be size of FRONTIER_VMAP_SIZE (if 
+        # necessary)
+        best_idxs = jnp.pad(best_idxs, 
+                            (0, max(0, FRONTIER_VMAP_SIZE - frontier_size)),
+                            mode='constant', constant_values=best_idxs[0])
+
+        # Exclude idxs beyond FRONTIER_VMAP_SIZE (if necessary)
+        best_idxs = best_idxs[:FRONTIER_VMAP_SIZE]
+
+        next_frontier = jax.tree_map(lambda x: x[best_idxs], total_frontier)
+
+        # The new queued_frontier is everything other than the best_idxs
+        queued_frontier = jax.tree_map(lambda x: jnp.delete(x, best_idxs, axis=0), total_frontier)
+        
+        print(f'iter {n_iter}, frontier size {frontier.done.shape} queued frontier size {queued_frontier.done.shape}')
 
 
     return best_state_actions, best_reward, n_iter_best, n_iter
