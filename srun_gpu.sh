657557d1a01f96f6e637a4e15839f36c81a6fb3e
Sam Earle <smearle93@gmail.com>
2024-05-17 18:14:24 -0400

    Compute no-op reward vals for saved elites.

--- srun_gpu.sh
+++ srun_gpu.sh
@@ -1,1 +1,1 @@
-srun -n1 --tasks=1 --cpus-per-task=1 -t1:00:00 --gres=gpu:rtx8000:1 --mem=30000 --pty /bin/bash
+srun -n1 --tasks=1 --cpus-per-task=1 -t1:00:00 --gres=gpu:1 --mem=30000 --pty /bin/bash
--- sweep_rl.py
+++ sweep_rl.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+import itertools
 from typing import Tuple
 
 import submitit
@@ -9,26 +11,37 @@ from rl_player_jax import main as train_rl
 # mode = 'local'
 mode = 'slurm'
 
+# hypers = {
+#     'load_gen': (5, 10, 15, 20, 65),
+#     # 'load_il': (True, False),
+#     'load_il': Fals
+#     'evo_freq': (-1, 1, 10),
+#     'n_train_envs': (1, 5, 10, 50, 100, -1),
+# }
+
+@dataclass
+class HyperParams:
+    load_gen: Tuple[int] = (5, 10, 15, 20, 65)
+    # load_il: Tuple[bool] = (True, False)
+    load_il: Tuple[bool] = (False,)
+    # evo_freq: Tuple[int] = (-1, 1, 10)
+    evo_freq: Tuple[int] = (-1,)
+    n_train_envs: Tuple[int] = (1, 5, 10, 50, 100, -1)
+
 
 def main():
+    hypers = HyperParams()
+    h_ks, h_vs = zip(*hypers.__dict__.items())
+    all_hyper_combos = list(itertools.product(*h_vs))
+    all_hyper_combos = [dict(zip(h_ks, h_v)) for h_v in all_hyper_combos]
     sweep_cfgs = []
-    for load_gen in (5, 10, 15, 20, 65):
-    # for load_gen in (65,):
-        for load_il in (True, False):
-        # for load_il in (False,):
-        # for load_il in (True,):
-            for evo_freq in (-1, 1, 10):
-            # for evo_freq in (-1,):
-            # for evo_freq in (1, 10):
-                cfg = RLConfig(
-                    load_gen=load_gen,
-                    load_il=load_il,
-                    evo_freq=evo_freq,
-                    env_exp_id=14,
-                    overwrite=True,
-                )
-                
-                sweep_cfgs.append(cfg)
+    for h in all_hyper_combos:
+        cfg = RLConfig(
+            **h,
+            env_exp_id=14,
+            overwrite=True,
+        )
+        sweep_cfgs.append(cfg)
 
     if mode == 'slurm':
         executor = submitit.AutoExecutor(folder='submitit_logs_rl')
